7155296837ef655b86fa22d9767403d86126c0de
marionsmlg <marion.schimmerling2@hotmail.fr>
2023-07-19 22:47:53 +0200

    test pages on phone

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -180,7 +180,7 @@ const bodyPart = [
         recettes adaptées à vous !
       </h1>
       <div>
-        <RouterLink to="/recipe"
+        <RouterLink to="/category/recipe"
           ><ButtonPrimary>Je cherche les recette qui me ressemble !</ButtonPrimary></RouterLink
         >
       </div>
