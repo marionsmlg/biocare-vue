e0342f132b3b8f898c58a5b11c2aefeb72ad1d2b
marionsmlg <marion.schimmerling2@hotmail.fr>
2023-10-04 00:05:43 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: process.env.PORT || 3000 // Utilisez le port défini par Railway ou le port 3000 par défaut
   }
 })
